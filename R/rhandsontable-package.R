826d3ae0625f2d2d79ec891ad48dfc9401d9ac7b
jrowen <jonathanro@gmail.com>
2015-03-06 15:40:27 -0600

    Initial commit

---
+++ R/rhandsontable-package.R
@@ -0,0 +1,22 @@
+#' An \code{htmlwidget} interface to the
+#' \href{http://http://handsontable.com//}{Handsontable.js} library
+#' @name rhandsontable
+#' @docType package
+#' @author Jonathan Owen
+#' @import htmlwidgets jsonlite
+NULL
+
+#' rhandsontable exported operators
+#'
+#' The following functions are imported and then re-exported
+#' from the dygraphs package to enable use of the magrittr
+#' pipe operator with no additional library calls
+#'
+#' @name rhandsontable-exports
+NULL
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname rhandsontable-exports
+NULL
