04ddf11de536181c7769afc6de9a568cb9d39bc5
Jonathan Owen <jonathanro@gmail.com>
2015-08-04 08:26:10 -0500

    Create types.R

---
+++ inst/examples/types.R
@@ -0,0 +1,20 @@
+library(rhandsontable)
+
+DF = data.frame(val = 1:10, 
+                bool = TRUE, 
+                big = LETTERS[1:10],
+                small = factor(letters[1:10]),
+                dt = seq(from = Sys.Date(), by = "days", length.out = 10),
+                stringsAsFactors = FALSE)
+
+rhandsontable(DF)
+
+DF2 = data.frame(val = c(NA, 2:10), 
+                bool = c(NA, rep(TRUE, 9)), 
+                big = c(NA, LETTERS[1:9]),
+                small = c(NA, factor(letters[1:9])),
+                dt = c(NA, seq(from = Sys.Date(), by = "days", length.out = 9)),
+                dt_ch = c(NA, as.character(seq(from = Sys.Date(), by = "days", length.out = 9))),
+                stringsAsFactors = FALSE)
+
+rhandsontable(DF2)
