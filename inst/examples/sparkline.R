d31095a198815267b8c61c309f07b2761e3aba37
jonathan <jonathanro@gmail.com>
2015-08-05 00:05:24 +0000

    Improved example

--- inst/examples/sparkline.R
+++ inst/examples/sparkline.R
@@ -5,7 +5,8 @@ DF = data.frame(val = 1:10, bool = TRUE, big = LETTERS[1:10],
                 dt = seq(from = Sys.Date(), by = "days", length.out = 10),
                 stringsAsFactors = FALSE)
 
-DF$chart = rep(jsonlite::toJSON(list(values=rnorm(10), 
-                                     options = list(type = "bar"))), 10)
+DF$chart = sapply(1:10,
+                  function(x) jsonlite::toJSON(list(values=rnorm(10),
+                                                    options = list(type = "bar"))))
 rhandsontable(DF, rowHeaders = NULL) %>%
   hot_col("chart", renderer = htmlwidgets::JS("renderSparkline"))
