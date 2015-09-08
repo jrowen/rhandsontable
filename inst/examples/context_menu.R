84b7cd2ac3c397159cb90705a895d1efc6bb8d42
jrowen <jonathanro@gmail.com>
2015-09-08 06:37:40 -0500

    Fixed example

--- inst/examples/context_menu.R
+++ inst/examples/context_menu.R
@@ -25,12 +25,12 @@ rhandsontable(DF) %>%
       csv = list(name = "Download to CSV",
                     callback = htmlwidgets::JS(
                       "function (key, options) {
-                         var csv = csvString(instance);
+                         var csv = csvString(this);
 
                          var link = document.createElement('a');
                          link.setAttribute('href', 'data:text/plain;charset=utf-8,' +
                            encodeURIComponent(csv));
-                         link.setAttribute('download', filename);
+                         link.setAttribute('download', 'data.csv');
 
                          document.body.appendChild(link);
                          link.click();
