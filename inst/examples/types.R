ac7a6a0aa693fdb53896e2c6cf5acd2f78640ea7
Jonathan Owen <jonthanro@gmail.com>
2016-07-04 13:13:56 +0000

    Doc clean-up and prep for cran

--- inst/examples/types.R
+++ inst/examples/types.R
@@ -1,7 +1,7 @@
 library(rhandsontable)
 
-DF = data.frame(val = 1:10, 
-                bool = TRUE, 
+DF = data.frame(val = 1:10,
+                bool = TRUE,
                 big = LETTERS[1:10],
                 small = factor(letters[1:10]),
                 dt = seq(from = Sys.Date(), by = "days", length.out = 10),
@@ -9,12 +9,12 @@ DF = data.frame(val = 1:10,
 
 rhandsontable(DF)
 
-DF2 = data.frame(val = c(NA, 2:10), 
-                bool = c(NA, rep(TRUE, 9)), 
-                big = c(NA, LETTERS[1:9]),
-                small = c(NA, factor(letters[1:9])),
-                dt = c(NA, seq(from = Sys.Date(), by = "days", length.out = 9)),
-                dt_ch = c(NA, as.character(seq(from = Sys.Date(), by = "days", length.out = 9))),
-                stringsAsFactors = FALSE)
+DF2 = data.frame(val = c(NA, 2:10),
+                 bool = c(NA, rep(TRUE, 9)),
+                 big = c(NA, LETTERS[1:9]),
+                 small = c(NA, factor(letters[1:9])),
+                 dt = c(NA, seq(from = Sys.Date(), by = "days", length.out = 9)),
+                 dt_ch = c(NA, as.character(seq(from = Sys.Date(), by = "days", length.out = 9))),
+                 stringsAsFactors = FALSE)
 
 rhandsontable(DF2)
