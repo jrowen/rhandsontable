d6098b8bb291a5f5d1520bc01b278f071877138d
jrowen <jonathanro@gmail.com>
2016-08-19 17:49:25 +0000

    Refresh after update

--- docs/libs/rhandsontable-0.3.1/rhandsontable.js
+++ docs/libs/rhandsontable-0.3.1/rhandsontable.js
@@ -49,18 +49,15 @@ HTMLWidgets.widget({
     this.afterRender(x);
 
     if (instance.hot) { // update existing instance
-
+      x.rInitInput = false;
+      instance.hot.params = x;
       instance.hot.updateSettings(x);
-      console.log("updating hot");
-
     } else {  // create new instance
-
       instance.hot = new Handsontable(el, x);
-      x.initInput = true;
-      console.log("creating hot");
+      x.rInitInput = true;
+      instance.hot.params = x;
+      instance.hot.updateSettings(x);
     }
-
-    instance.hot.params = x;
   },
 
   resize: function(el, width, height, instance) {
@@ -84,8 +81,10 @@ HTMLWidgets.widget({
   afterChangeCallback: function(x) {
 
     x.afterChange = function(changes, source) {
+      console.log(source);
       if (HTMLWidgets.shinyMode) {
         if (changes && changes[0][2] !== null && changes[0][3] !== null) {
+          console.log("change");
           if (this.sortIndex && this.sortIndex.length !== 0) {
             c = [this.sortIndex[changes[0][0]][0], changes[0].slice(1, 1 + 3)];
           } else {
@@ -97,8 +96,9 @@ HTMLWidgets.widget({
             changes: { event: "afterChange", changes: c, source: source },
             params: this.params
           });
-        } else if (source == "loadData" && this.params.initInput) {
-          this.params.initInput = false;
+        } else if (source == "loadData" && this.params && this.params.rInitInput) {
+          this.rInitInput = false;
+
           Shiny.onInputChange(this.rootElement.id, {
             data: this.getData(),
             changes: { event: "afterChange", changes: null },
