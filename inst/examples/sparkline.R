02bc2921a8787ef0ef8f822154616758edafe4ec
Jonathan Owen <jonathanro@gmail.com>
2018-01-20 06:27:37 -0600

    Updated sparkline examples

--- inst/examples/sparkline.R
+++ inst/examples/sparkline.R
@@ -10,3 +10,26 @@ DF$chart = sapply(1:10,
                                                     options = list(type = "bar"))))
 rhandsontable(DF, rowHeaders = NULL) %>%
   hot_col("chart", renderer = htmlwidgets::JS("renderSparkline"))
+
+
+spk_df <- data.frame(
+  value = 1:2,
+  factor = c("A", "B"),
+  chart = sapply(
+    list(
+      list(
+        values=c(3,3,3,-3,-3,-3),
+        options=list(type="bar", zeroAxis=FALSE)
+      ),
+      list(
+        values=runif(10, -3, 3),
+        options=list(type="bar", zeroAxis=FALSE, barColor="purple", negBarColor="yellow")
+      )
+    ),
+    jsonlite::toJSON
+  ),
+  stringsAsFactors = FALSE
+)
+
+rhandsontable(spk_df, rowHeaders = NULL, width = 550, height = 300) %>%
+  hot_col("chart", renderer = htmlwidgets::JS("renderSparkline"))
