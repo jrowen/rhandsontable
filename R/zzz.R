18b765dd2aa23040b99d4fb120ce021b410b2051
Paul-A-Kavan <30735308+Paul-A-Kavan@users.noreply.github.com>
2018-07-24 20:18:20 -0500

    Create zzz.R to add .onLoad
    Added custom deserializer for the selectCallback's returned data object.

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onLoad <- function(...) {
+
+  shiny::registerInputHandler("rhandsontable.customSelectDeserializer", 
+                              function(x, session, inputName) {
+                                result <- x
+                                result$select$rAll <- unlist(x$select$rAll)
+                                result$select$cAll <- unlist(x$select$cAll)
+                                result}, 
+                              force = TRUE )
+    
+}
