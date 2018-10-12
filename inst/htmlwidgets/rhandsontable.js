dae021f25d4ea9add21fced61784da3074f67f6a
Jonathan Owen <jonathanro@gmail.com>
2018-10-11 19:49:53 -0500

    Initial updates

--- inst/htmlwidgets/rhandsontable.js
+++ inst/htmlwidgets/rhandsontable.js
@@ -6,318 +6,316 @@ HTMLWidgets.widget({
 
   params: null,
 
-  initialize: function(el, width, height) {
+  factory: function(el, width, height) {
+
+    var hot = null;
 
     return {
+      renderValue: function(x) {
 
-    };
-
-  },
-
-  renderValue: function(el, x, instance) {
-
-    // convert json to array
-    if (x.data.length > 0 && x.data[0].constructor === Array) {
-      x.data = x.data;
-    } else {
-      x.data = toArray(x.data.map(function(d) {
-        return x.rColnames.map(function(ky) {
-          return d[ky];
-        });
-      }));
-    }
-
-    if (x.isHeatmap === true) {
-      x.afterLoadData = this.initHeatmap;
-      x.beforeChangeRender = this.updateHeatmap;
-    }
-
-    if (x.overflow) {
-      $("#" + el.id).css('overflow', x.overflow);
-    }
-
-    if (x.rowHeaderWidth) {
-      $("#" + el.id).css('col.rowHeader', x.rowHeaderWidth + 'px');
-    }
-
-    //this.afterRender(x);
-
-    this.params = x;
-
-    if (instance.hot) { // update existing instance
-      if (x.debug && x.debug > 0) {
-        console.log("rhandsontable: update table");
-      }
-
-      instance.hot.params = x;
-      instance.hot.updateSettings(x);
-    } else {  // create new instance
-      if (x.debug && x.debug > 0) {
-        console.log("rhandsontable: new table");
-      }
-
-      instance.hot = new Handsontable(el, x);
-
-      this.afterChangeCallback(x);
-      this.afterCellMetaCallback(x);
-      this.afterRowAndColChange(x);
-
-      if (x.selectCallback) {
-        this.afterSelectCallback(x);
-      }
-
-      instance.hot.params = x;
-      instance.hot.updateSettings(x);
-
-      var searchField = document.getElementById('searchField');
-      if (typeof(searchField) != 'undefined' && searchField != null) {
-        Handsontable.dom.addEvent(searchField, 'keyup', function (event) {
-          var queryResult = instance.hot.search.query(this.value);
-          instance.hot.render();
-        });
-      }
-    }
-  },
-
-  resize: function(el, width, height, instance) {
-
-  },
-
-  afterRender: function(x) {
-    x.afterRender = function(isForced) {
-      var plugin = this.getPlugin('autoColumnSize');
-      if (plugin.isEnabled() && this.params) {
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("rhandsontable: resizing column widths");
-          }
+        // convert json to array
+        if (x.data.length > 0 && x.data[0].constructor === Array) {
+          x.data = x.data;
+        } else {
+          x.data = toArray(x.data.map(function(d) {
+            return x.rColnames.map(function(ky) {
+              return d[ky];
+            });
+          }));
         }
 
-        var wdths = plugin.widths;
+        if (x.isHeatmap === true) {
+          x.afterLoadData = this.initHeatmap;
+          x.beforeChangeRender = this.updateHeatmap;
+        }
+
+        if (x.overflow) {
+          $("#" + el.id).css('overflow', x.overflow);
+        }
+
+        if (x.rowHeaderWidth) {
+          $("#" + el.id).css('col.rowHeader', x.rowHeaderWidth + 'px');
+        }
+
+        //this.afterRender(x);
+
+        this.params = x;
+
+        if (hot) { // update existing instance
+          if (x.debug && x.debug > 0) {
+            console.log("rhandsontable: update table");
+          }
+
+          hot.params = x;
+          hot.updateSettings(x);
+        } else {  // create new instance
+          if (x.debug && x.debug > 0) {
+            console.log("rhandsontable: new table");
+          }
+
+          hot = new Handsontable(el, x);
+
+          this.afterChangeCallback(x);
+          this.afterCellMetaCallback(x);
+          this.afterRowAndColChange(x);
+
+          if (x.selectCallback) {
+            this.afterSelectCallback(x);
+          }
+
+          hot.params = x;
+          hot.updateSettings(x);
+
+          var searchField = document.getElementById('searchField');
+          if (typeof(searchField) != 'undefined' && searchField !== null) {
+            Handsontable.dom.addEvent(searchField, 'keyup', function (event) {
+              var queryResult = hot.search.query(this.value);
+              hot.render();
+            });
+          }
+        }
+      },
+
+      resize: function(width, height, instance) {
+
+      },
+
+      afterRender: function(x) {
+        x.afterRender = function(isForced) {
+          var plugin = this.getPlugin('autoColumnSize');
+          if (plugin.isEnabled() && this.params) {
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("rhandsontable: resizing column widths");
+              }
+            }
+
+            var wdths = plugin.widths;
+            for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
+              if (this.params.columns && this.params.columns[i].renderer.name != "customRenderer") {
+                plugin.calculateColumnsWidth(i, 300, true);
+              }
+            }
+          }
+        };
+      },
+
+      afterChangeCallback: function(x) {
+
+        x.afterChange = function(changes, source) {
+          if (this.params && this.params.debug) {
+            if (this.params.debug > 0) {
+              console.log("afterChange: " + source);
+            }
+            if (this.params.debug > 1) {
+              console.log("afterChange:");
+              console.log(changes);
+            }
+          }
+
+          if (HTMLWidgets.shinyMode) {
+            if (changes && (changes[0][2] !== null || changes[0][3] !== null)) {
+              if (this.sortIndex && this.sortIndex.length !== 0) {
+                c = [this.sortIndex[changes[0][0]][0], changes[0].slice(1, 1 + 3)];
+              } else {
+                c = changes;
+              }
+
+              if (this.params && this.params.debug) {
+                if (this.params.debug > 0) {
+                  console.log("afterChange: Shiny.onInputChange: " + this.rootElement.id);
+                }
+              }
+              Shiny.onInputChange(this.rootElement.id, {
+                data: this.getData(),
+                changes: { event: "afterChange", changes: c, source: source },
+                params: this.params
+              });
+            } else if (source == "loadData" && this.params) {
+
+              if (this.params && this.params.debug) {
+                if (this.params.debug > 0) {
+                  console.log("afterChange: Shiny.onInputChange: " + this.rootElement.id);
+                }
+              }
+              // push input change to shiny so input$hot and output$hot are in sync (see #137)
+              Shiny.onInputChange(this.rootElement.id, {
+                data: this.getData(),
+                changes: { event: "afterChange", changes: null },
+                params: this.params
+              });
+            }
+          }
+
+        };
+
+        x.afterLoadData = function(firstTime) {
+          if (this.params && this.params.debug) {
+            if (this.params.debug > 0) {
+              console.log("afterLoadData: " + firstTime);
+            }
+          }
+        };
+
+        x.afterChangesObserved = function(firstTime) {
+          if (this.params && this.params.debug) {
+            if (this.params.debug > 0) {
+              console.log("afterChangesObserved");
+            }
+          }
+        };
+
+        x.afterInit = function() {
+          if (this.params && this.params.debug) {
+            if (this.params.debug > 0) {
+              console.log("afterInit");
+            }
+          }
+        };
+      },
+
+      afterCellMetaCallback: function(x) {
+
+        x.afterSetCellMeta = function(r, c, key, val) {
+
+          if (HTMLWidgets.shinyMode && key === "comment") {
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterSetCellMeta: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id + "_comment", {
+              data: this.getData(),
+              comment: { r: r + 1, c: c + 1, key: key, val: val},
+              params: this.params
+            });
+          }
+
+        };
+      },
+
+      afterSelectCallback: function(x) {
+
+        x.afterSelectionEnd = function(r, c, r2, c2) {
+
+          var r_all = [];
+          var c_all = [];
+          if (HTMLWidgets.shinyMode) {
+            if ( r2 < r ) { r2 = [r, r = r2][0]; }
+            for ( var i=r; i <= r2; i++ ) {
+              r_all.push( this.toPhysicalRow(i) + 1 );
+            }
+            if ( c2 < c ) { c2 = [c, c = c2][0]; }
+            for ( var i=c; i <= c2; i++ ) {
+              c_all.push( this.toPhysicalColumn(i) + 1 );
+            }
+
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterSelectionEnd: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id + "_select:rhandsontable.customSelectDeserializer", {
+              data: this.getData(),
+              select: { r: r + 1,
+                        c: c + 1,
+                        r2: r2 + 1,
+                        c2: c2 + 1,
+                        rAll: r_all,
+                        cAll: c_all },
+              params: this.params
+            });
+          }
+
+        };
+      },
+
+      afterRowAndColChange: function(x) {
+
+        x.afterCreateRow = function(ind, ct) {
+
+          if (HTMLWidgets.shinyMode) {
+
+            if (this.params && this.params.columns) {
+              for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
+                this.setDataAtCell(ind, i, this.params.columns[i].default);
+              }
+            }
+
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterCreateRow: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id, {
+              data: this.getData(),
+              changes: { event: "afterCreateRow", ind: ind, ct: ct },
+              params: this.params
+            });
+          }
+        };
+
+        x.afterRemoveRow = function(ind, ct) {
+
+          if (HTMLWidgets.shinyMode)
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterRemoveRow: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id, {
+              data: this.getData(),
+              changes: { event: "afterRemoveRow", ind: ind, ct: ct },
+              params: this.params
+            });
+        };
+
+        x.afterCreateCol = function(ind, ct) {
+
+          if (HTMLWidgets.shinyMode)
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterCreateCol: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id, {
+              data: this.getData(),
+              changes: { event: "afterCreateCol", ind: ind, ct: ct },
+              params: this.params
+            });
+        };
+
+        x.afterRemoveCol = function(ind, ct) {
+
+          if (HTMLWidgets.shinyMode)
+            if (this.params && this.params.debug) {
+              if (this.params.debug > 0) {
+                console.log("afterRemoveCol: Shiny.onInputChange: " + this.rootElement.id);
+              }
+            }
+            Shiny.onInputChange(this.rootElement.id, {
+              data: this.getData(),
+              changes: { event: "afterRemoveCol", ind: ind, ct: ct },
+              params: this.params
+            });
+        };
+
+      },
+
+      // see http://handsontable.com/demo/heatmaps.html
+      initHeatmap: function(firstTime, source) {
+        this.heatmap = [];
+
         for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
-          if (this.params.columns && this.params.columns[i].renderer.name != "customRenderer") {
-            plugin.calculateColumnsWidth(i, 300, true);
-          }
+          this.heatmap[i] = generateHeatmapData.call(this, i);
         }
+      },
+
+      updateHeatmap: function(change, source) {
+        this.heatmap[change[0][1]] = generateHeatmapData.call(this, change[0][1]);
       }
     };
-  },
-
-  afterChangeCallback: function(x) {
-
-    x.afterChange = function(changes, source) {
-      if (this.params && this.params.debug) {
-        if (this.params.debug > 0) {
-          console.log("afterChange: " + source);
-        }
-        if (this.params.debug > 1) {
-          console.log("afterChange:");
-          console.log(changes);
-        }
-      }
-
-      if (HTMLWidgets.shinyMode) {
-        if (changes && (changes[0][2] !== null || changes[0][3] !== null)) {
-          if (this.sortIndex && this.sortIndex.length !== 0) {
-            c = [this.sortIndex[changes[0][0]][0], changes[0].slice(1, 1 + 3)];
-          } else {
-            c = changes;
-          }
-
-          if (this.params && this.params.debug) {
-            if (this.params.debug > 0) {
-              console.log("afterChange: Shiny.onInputChange: " + this.rootElement.id);
-            }
-          }
-          Shiny.onInputChange(this.rootElement.id, {
-            data: this.getData(),
-            changes: { event: "afterChange", changes: c, source: source },
-            params: this.params
-          });
-        } else if (source == "loadData" && this.params) {
-
-          if (this.params && this.params.debug) {
-            if (this.params.debug > 0) {
-              console.log("afterChange: Shiny.onInputChange: " + this.rootElement.id);
-            }
-          }
-          // push input change to shiny so input$hot and output$hot are in sync (see #137)
-          Shiny.onInputChange(this.rootElement.id, {
-            data: this.getData(),
-            changes: { event: "afterChange", changes: null },
-            params: this.params
-          });
-        }
-      }
-
-    };
-
-    x.afterLoadData = function(firstTime) {
-      if (this.params && this.params.debug) {
-        if (this.params.debug > 0) {
-          console.log("afterLoadData: " + firstTime);
-        }
-      }
-    };
-
-    x.afterChangesObserved = function(firstTime) {
-      if (this.params && this.params.debug) {
-        if (this.params.debug > 0) {
-          console.log("afterChangesObserved");
-        }
-      }
-    };
-
-    x.afterInit = function() {
-      if (this.params && this.params.debug) {
-        if (this.params.debug > 0) {
-          console.log("afterInit");
-        }
-      }
-    };
-  },
-
-  afterCellMetaCallback: function(x) {
-
-    x.afterSetCellMeta = function(r, c, key, val) {
-
-      if (HTMLWidgets.shinyMode && key === "comment") {
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterSetCellMeta: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id + "_comment", {
-          data: this.getData(),
-          comment: { r: r + 1, c: c + 1, key: key, val: val},
-          params: this.params
-        });
-      }
-
-    };
-  },
-
-  afterSelectCallback: function(x) {
-    
-    x.afterSelectionEnd = function(r, c, r2, c2) {
-
-      var r_all = [];
-      var c_all = [];
-      if (HTMLWidgets.shinyMode) {
-        if ( r2 < r ) { r2 = [r, r = r2][0]; }
-        for ( var i=r; i <= r2; i++ ) {
-          r_all.push( this.toPhysicalRow(i) + 1 );
-        }
-        if ( c2 < c ) { c2 = [c, c = c2][0]; }
-        for ( var i=c; i <= c2; i++ ) {
-          c_all.push( this.toPhysicalColumn(i) + 1 );
-        }
-
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterSelectionEnd: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id + "_select:rhandsontable.customSelectDeserializer", {
-          data: this.getData(),
-          select: { r: r + 1, 
-                    c: c + 1, 
-                    r2: r2 + 1, 
-                    c2: c2 + 1,
-                    rAll: r_all,
-                    cAll: c_all },
-          params: this.params
-        });
-      }
-
-    };
-  },
-
-  afterRowAndColChange: function(x) {
-
-    x.afterCreateRow = function(ind, ct) {
-
-      if (HTMLWidgets.shinyMode) {
-
-        if (this.params && this.params.columns) {
-          for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
-            this.setDataAtCell(ind, i, this.params.columns[i].default);
-          }
-        }
-
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterCreateRow: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id, {
-          data: this.getData(),
-          changes: { event: "afterCreateRow", ind: ind, ct: ct },
-          params: this.params
-        });
-      }
-    };
-
-    x.afterRemoveRow = function(ind, ct) {
-
-      if (HTMLWidgets.shinyMode)
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterRemoveRow: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id, {
-          data: this.getData(),
-          changes: { event: "afterRemoveRow", ind: ind, ct: ct },
-          params: this.params
-        });
-    };
-
-    x.afterCreateCol = function(ind, ct) {
-
-      if (HTMLWidgets.shinyMode)
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterCreateCol: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id, {
-          data: this.getData(),
-          changes: { event: "afterCreateCol", ind: ind, ct: ct },
-          params: this.params
-        });
-    };
-
-    x.afterRemoveCol = function(ind, ct) {
-
-      if (HTMLWidgets.shinyMode)
-        if (this.params && this.params.debug) {
-          if (this.params.debug > 0) {
-            console.log("afterRemoveCol: Shiny.onInputChange: " + this.rootElement.id);
-          }
-        }
-        Shiny.onInputChange(this.rootElement.id, {
-          data: this.getData(),
-          changes: { event: "afterRemoveCol", ind: ind, ct: ct },
-          params: this.params
-        });
-    };
-
-  },
-
-  // see http://handsontable.com/demo/heatmaps.html
-  initHeatmap: function(firstTime, source) {
-    this.heatmap = [];
-
-    for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
-      this.heatmap[i] = generateHeatmapData.call(this, i);
-    }
-  },
-
-  updateHeatmap: function(change, source) {
-    this.heatmap[change[0][1]] = generateHeatmapData.call(this, change[0][1]);
   }
-
 });
 
 function generateHeatmapData(colId) {
