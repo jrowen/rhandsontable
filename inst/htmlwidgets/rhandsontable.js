4972363daf7d4e5e0c319358bec86696f3691dcb
ericnewkirk <55846615+ericnewkirk@users.noreply.github.com>
2021-02-04 12:04:51 -0700

    Force previous fix
    Fix for minsparerows is stuck

--- inst/htmlwidgets/rhandsontable.js
+++ inst/htmlwidgets/rhandsontable.js
@@ -242,7 +242,11 @@ HTMLWidgets.widget({
 
         if (this.params && this.params.columns) {
           for(var i = 0, colCount = this.countCols(); i < colCount ; i++) {
-            this.setDataAtCell(ind, i, this.params.columns[i].default);
+            // added to prevent phantom change event - change will still fire
+            // if default values are provided
+            if (this.params.columns[1].default !== null) {
+              this.setDataAtCell(ind, i, this.params.columns[i].default);
+            }
           }
         }
 
